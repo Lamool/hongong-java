35f8df23c6abb742957d422adfc928185d426668
Lamool <gosu0522@naver.com>
2025-08-20 00:03:26 +0900

    250819_상속

--- src/chapter07/sec01/exam07/pack2/D.java
+++ src/chapter07/sec01/exam07/pack2/D.java
@@ -4,15 +4,17 @@ import chapter07.sec01.exam07.pack1.A;
 
 public class D extends A {
     public D() {
-        super();
-        this.field = "value";
-        this.method();
-    }
-
-    /*
+        /*
         A 클래스와 다른 패키지.
         A의 자식 클래스기 때문에 A 클래스의 protected 필드, 생성자, 메소드에 접근 가능
         단, new 연산자를 사용해서 생성자를 직접 호출할 수는 없다.
         자식 생성자에서 super()로 A 생성자를 호출할 수 있다.
-    */
+
+        super();
+        this.field = "value";
+        this.method();
+        */
+    }
+
+
 }
